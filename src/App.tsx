ffffaa0a254f52650a8016327c31f83dac3204a0
AleksandrFROLik <nextfrolik@gmail.com>
2022-01-24 23:37:21 +0300

    create components

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
 import React from 'react';
 import './App.css';
+import {BrowserRouter} from "react-router-dom";
+import Login from "./pages/Login";
+import Registration from "./pages/Registration";
+import Profile from "./pages/Profile";
+import Error404 from "./pages/Error404";
+import PasswordRecovery from "./pages/PasswordRecovery";
+import EnterNewPassword from "./pages/EnterNewPassword";
+import TestPage from "./pages/TestPage";
 
 function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
+    return (
+        <BrowserRouter>
+            <Login/>
+            <Registration/>
+            <Profile/>
+            <Error404/>
+            <PasswordRecovery/>
+            <EnterNewPassword/>
+            <TestPage/>
+        </BrowserRouter>
+    );
 }
-
 export default App;
---
+++ src/pages/TestPage.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import SuperButton from "../commponents/c2-SuperButton/SuperButton";
+import SuperCheckbox from "../commponents/c3-SuperCheckbox/SuperCheckbox";
+import SuperInputText from "../commponents/c1-SuperInputText/SuperInputText";
+
+const TestPage = () => {
+    return (
+        <div>
+            <SuperButton/>
+            <SuperCheckbox/>
+            <SuperInputText/>
+        </div>
+    );
+};
+
+export default TestPage;
